5f4b6d4cfb0c0898affe26a75da5af388b45214c
lotfio <contact@lotfio.net>
2019-10-20 20:46:04 +0100

    update view render method using caprice 0.2

--- src/Http/View.php
+++ src/Http/View.php
@@ -13,10 +13,21 @@ use OoFile\Conf;
 
 class View
 {
+    /**
+     * render view method using caprice templating engine
+     * 
+     * @param  string $file
+     * @param  mixed  $data
+     * @return void
+     */
     public function render(string $file, $data = NULL)
     {
-        $caprice  = new Compiler;
-        $compiled = $caprice->compile(Conf::path("views") . $file , Conf::path("cache") . "views");
+        $caprice  = new Compiler(Conf::path("views"), Conf::path("cache") . "views");
+
+        if(env("APP_ENV") != "dev")
+            $caprice->setProductionMode();
+
+        $compiled = $caprice->compile($file);
         $data     = $data;
         require $compiled;
     }
